d834d2f0f1df8fec1dfa7a24e0a51baea4de3291
mychelle-comput <mychelinha7@hotmail.com>
2024-03-24 15:44:42 -0300

    Cria as entidades medico e endereco

---
+++ src/main/java/br/com/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,20 @@
+package br.com.med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logadouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
---
+++ src/main/java/br/com/med/voll/api/medico/Medico.java
@@ -0,0 +1,31 @@
+package br.com.med.voll.api.medico;
+
+import br.com.med.voll.api.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+
+
+
+}
