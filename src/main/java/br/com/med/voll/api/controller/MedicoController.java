025fa959aa55b850530988ace4294dadb1649448
mychelle-comput <mychelinha7@hotmail.com>
2024-03-26 14:46:25 -0300

    Adicionei a Dependência do Flyway e ultilizei o Bean Validation para validar os campos

--- src/main/java/br/com/med/voll/api/controller/MedicoController.java
+++ src/main/java/br/com/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package br.com.med.voll.api.controller;
 
 import br.com.med.voll.api.medico.DadosCadastroMedico;
+import br.com.med.voll.api.medico.Medico;
+import br.com.med.voll.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
     }
 
 
---
+++ src/main/java/br/com/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,9 @@
+package br.com.med.voll.api.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository <Medico, Long>{
+
+
+
+}
