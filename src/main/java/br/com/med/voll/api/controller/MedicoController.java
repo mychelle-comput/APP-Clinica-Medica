b57ae263c55c793e08d3dc84a016eb400592657d
mychelle-comput <mychelinha7@hotmail.com>
2024-03-27 19:22:07 -0300

    Adicionei um Get Mapping para listagem de Médicos.

--- src/main/java/br/com/med/voll/api/controller/MedicoController.java
+++ src/main/java/br/com/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package br.com.med.voll.api.controller;
 
 import br.com.med.voll.api.medico.DadosCadastroMedico;
+import br.com.med.voll.api.medico.DadosListagemMedico;
 import br.com.med.voll.api.medico.Medico;
 import br.com.med.voll.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -23,7 +23,9 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
-
-
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
 
 }
