b8274d4dc8b164d22555c8cd70901d3bab173270
mychelle-comput <mychelinha7@hotmail.com>
2024-03-29 20:40:48 -0300

    Adicionei um Get Mapping para listagem de pacientes.

--- src/main/java/br/com/med/voll/api/controller/PacienteController.java
+++ src/main/java/br/com/med/voll/api/controller/PacienteController.java
@@ -2,14 +2,15 @@ package br.com.med.voll.api.controller;
 
 
 import br.com.med.voll.api.paciente.DadosCadastroPacientes;
+import br.com.med.voll.api.paciente.DadosListagemPacientes;
 import br.com.med.voll.api.paciente.Paciente;
 import br.com.med.voll.api.paciente.PacienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -22,6 +23,11 @@ public class PacienteController {
     public void cadastroPacientes(@RequestBody @Valid DadosCadastroPacientes dados){
         repository.save(new Paciente(dados));
 
+    }
+
+    @GetMapping
+    public Page<DadosListagemPacientes> listar(@PageableDefault(size = 10, sort={"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPacientes::new);
     }
 
 
